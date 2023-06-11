4eae6a42a50cc41d2eca3ca8088ab147ffcb41be
fleonardolewis@gmail.com <fleonardolewis@gmail.com>
2023-06-11 17:56:57 -0500

    RAPM chart

--- data/pwpha/pwpha_rapm.py
+++ data/pwpha/pwpha_rapm.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import Ridge
+import matplotlib.pyplot as plt
+import numpy as np
 
 pd.options.display.max_columns = None
 
@@ -8,7 +10,7 @@ f = pd.ExcelFile('23_SUMMARY_WHKYHAC_SPORTLOGIQ.xlsx')
 
 pw = pd.read_excel(f,'SkatersPW')
 
-pw['xG diff/60'] = (pw['XGF WOI (AA Model)'] - pw['XGA WOI (AA Model)']) / ((pw['TOI'] / 60) / 60)
+pw['xG diff/60'] = ((pw['XGF WOI (AA Model)'] - pw['XGA WOI (AA Model)']) / (pw['TOI'] / 1000)) * 60
 
 print(pw[['Player', 'TOI', 'XGF WOI (AA Model)', 'XGA WOI (AA Model)', 'xG diff/60']])
 
@@ -28,4 +30,15 @@ coef_table.insert(len(coef_table.columns),"Coefs",rdg.coef_.transpose())
 
 print(coef_table.sort_values(by=['Coefs']))
 
-print(pw['Player'].loc[pw['Player'] == 'Amanda Kessel'])
+std = coef_table['Coefs'].std()
+avg = coef_table['Coefs'].mean()
+
+coef_table['z_Score'] = (coef_table['Coefs'] - avg) / std
+coef_table['color'] = np.where(coef_table['z_Score'] > 0, 'blue', 'red')
+print(coef_table.sort_values(by=['Coefs']))
+
+df = pd.DataFrame(coef_table[[0,'z_Score']])
+
+ax = df.plot.bar(x=0, y='z_Score', color=coef_table['color'], rot=0)
+plt.xticks(rotation='vertical')
+plt.show()
