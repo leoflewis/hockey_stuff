b8aa3dfe5f5f1b12094e1b987f912b66a9df9e50
fleonardolewis@gmail.com <fleonardolewis@gmail.com>
2023-08-12 11:28:27 -0500

    Game model data

--- modeling/game_data.py
+++ modeling/game_data.py
@@ -1,6 +1,124 @@
-import mysql.connector, requests, pandas, numpy, math, os
+import mysql.connector, requests, pandas as pd, numpy, math, os
 from mysql.connector import Error
 from joblib import load
+
+def get_goals(date, home, away, cursor):
+    home_awayxG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE EventTeam = {} AND GameDate < '{}' and EventName = 'GOAL';".format(home, date)
+    cursor.execute(home_awayxG_query)
+    home_xGf = cursor.fetchall()
+
+    away_homexG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE EventTeam = {} AND GameDate < '{}' and EventName = 'GOAL';".format(away, date)
+    cursor.execute(away_homexG_query)
+    away_xGf = cursor.fetchall()
+
+    home_awayxG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE ConcededTeam = {} AND GameDate < '{}' and EventName = 'GOAL';".format(home, date)
+    cursor.execute(home_awayxG_query)
+    home_xGa = cursor.fetchall()
+
+    away_homexG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE ConcededTeam = {} AND GameDate < '{}' and EventName = 'GOAL';".format(away, date)
+    cursor.execute(away_homexG_query)
+    away_xGa = cursor.fetchall()
+    hxgf = home_xGf[0][0]
+    hxga = home_xGa[0][0]
+    axgf = away_xGf[0][0]
+    axga = away_xGa[0][0]
+    if hxgf == None: hxgf = 0
+    if hxga == None: hxga = 0
+    if axgf == None: axgf = 0
+    if axga == None: axga = 0
+    homexGDiffToDate =  hxgf - hxga
+    awayxGDiffToDate =  axgf - axga
+
+    return homexGDiffToDate, awayxGDiffToDate
+
+def get_shots(date, home, away, cursor):
+    home_awayxG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE EventTeam = {} AND GameDate < '{}' and EventName = 'SHOT';".format(home, date)
+    cursor.execute(home_awayxG_query)
+    home_xGf = cursor.fetchall()
+
+    away_homexG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE EventTeam = {} AND GameDate < '{}' and EventName = 'SHOT';".format(away, date)
+    cursor.execute(away_homexG_query)
+    away_xGf = cursor.fetchall()
+
+    home_awayxG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE ConcededTeam = {} AND GameDate < '{}' and EventName = 'SHOT';".format(home, date)
+    cursor.execute(home_awayxG_query)
+    home_xGa = cursor.fetchall()
+
+    away_homexG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE ConcededTeam = {} AND GameDate < '{}' and EventName = 'SHOT';".format(away, date)
+    cursor.execute(away_homexG_query)
+    away_xGa = cursor.fetchall()
+    hxgf = home_xGf[0][0]
+    hxga = home_xGa[0][0]
+    axgf = away_xGf[0][0]
+    axga = away_xGa[0][0]
+    if hxgf == None: hxgf = 0
+    if hxga == None: hxga = 0
+    if axgf == None: axgf = 0
+    if axga == None: axga = 0
+    homexGDiffToDate =  hxgf - hxga
+    awayxGDiffToDate =  axgf - axga
+
+    return homexGDiffToDate, awayxGDiffToDate
+
+def get_fenwick(date, home, away, cursor):
+    home_awayxG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE EventTeam = {} AND GameDate < '{}';".format(home, date)
+    cursor.execute(home_awayxG_query)
+    home_xGf = cursor.fetchall()
+
+    away_homexG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE EventTeam = {} AND GameDate < '{}';".format(away, date)
+    cursor.execute(away_homexG_query)
+    away_xGf = cursor.fetchall()
+
+    home_awayxG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE ConcededTeam = {} AND GameDate < '{}';".format(home, date)
+    cursor.execute(home_awayxG_query)
+    home_xGa = cursor.fetchall()
+
+    away_homexG_query = "SELECT count(*) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE ConcededTeam = {} AND GameDate < '{}';".format(away, date)
+    cursor.execute(away_homexG_query)
+    away_xGa = cursor.fetchall()
+    hxgf = home_xGf[0][0]
+    hxga = home_xGa[0][0]
+    axgf = away_xGf[0][0]
+    axga = away_xGa[0][0]
+    if hxgf == None: hxgf = 0
+    if hxga == None: hxga = 0
+    if axgf == None: axgf = 0
+    if axga == None: axga = 0
+    homexGDiffToDate =  hxgf - hxga
+    awayxGDiffToDate =  axgf - axga
+
+    return homexGDiffToDate, awayxGDiffToDate
+
+def get_xg(date, home, away, cursor):
+    home_awayxG_query = "SELECT sum(xG) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE EventTeam = {} AND GameDate < '{}';".format(home, date)
+    cursor.execute(home_awayxG_query)
+    home_xGf = cursor.fetchall()
+
+    away_homexG_query = "SELECT sum(xG) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE EventTeam = {} AND GameDate < '{}';".format(away, date)
+    cursor.execute(away_homexG_query)
+    away_xGf = cursor.fetchall()
+
+    home_awayxG_query = "SELECT sum(xG) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE ConcededTeam = {} AND GameDate < '{}';".format(home, date)
+    cursor.execute(home_awayxG_query)
+    home_xGa = cursor.fetchall()
+
+    away_homexG_query = "SELECT sum(xG) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE ConcededTeam = {} AND GameDate < '{}';".format(away, date)
+    cursor.execute(away_homexG_query)
+    away_xGa = cursor.fetchall()
+    hxgf = home_xGf[0][0]
+    hxga = home_xGa[0][0]
+    axgf = away_xGf[0][0]
+    axga = away_xGa[0][0]
+    if hxgf == None: hxgf = 0
+    if hxga == None: hxga = 0
+    if axgf == None: axgf = 0
+    if axga == None: axga = 0
+    homexGDiffToDate =  hxgf - hxga
+    awayxGDiffToDate =  axgf - axga
+
+    return homexGDiffToDate, awayxGDiffToDate
+
+
 db = None
 try:
     db = mysql.connector.connect(
@@ -13,6 +131,9 @@ try:
 except Error as e:
     print("Error while connecting to MySQL", e) 
 
+
+df = pd.DataFrame(columns=['homexGDiff','awayxGdiff','homeShotDiff','awayShotDiff', 'homeFenDiff', 'awayFenDiff', 'homeGoalDiff', 'awayGoalDiff', 'homeWin'])
+
 if db.is_connected():
     get_games = "SELECT * FROM Game;"
     cursor = db.cursor()
@@ -21,29 +142,22 @@ if db.is_connected():
         cursor.execute(get_games)
         games = cursor.fetchall()
         for game in games:
-            print()
-            print(game[2])
-            print("*** NEW GAME ***")
+            homeWin = game[12]
+            
+            homexGDiffToDate, awayxGDiffToDate = get_xg(game[2], game[7], game[8], cursor)
+            
+            homeShotDiffToDate, awayShotDiffToDate = get_shots(game[2], game[7], game[8], cursor)
+            
+            homeGoalDiffToDate, awayGoalDiffToDate = get_goals(game[2], game[7], game[8], cursor)
+            
+            homefenDiffToDate, awayFenDiffToDate = get_fenwick(game[2], game[7], game[8], cursor)
+            
+            df.loc[len(df)]= [homexGDiffToDate, awayxGDiffToDate, homeShotDiffToDate, awayShotDiffToDate, homefenDiffToDate, awayFenDiffToDate, homeGoalDiffToDate, awayGoalDiffToDate, homeWin]
+            print("added row for game: {}".format(game[0]))
+        
+        df.to_excel("GameData.xlsx")
+            
 
-            home_awayxG_query = "SELECT sum(xG) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE EventTeam = {} AND GameDate < '{}';".format(game[7], game[2])
-            cursor.execute(home_awayxG_query)
-            home_xGf = cursor.fetchall()
-            print("Home team to date xGF: " + str(home_xGf[0][0]))
-
-            away_homexG_query = "SELECT sum(xG) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE EventTeam = {} AND GameDate < '{}';".format(game[8], game[2])
-            cursor.execute(away_homexG_query)
-            away_xGf = cursor.fetchall()
-            print("Away team to date xGF: " + str(away_xGf[0][0]))
-
-            home_awayxG_query = "SELECT sum(xG) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE ConcededTeam = {} AND GameDate < '{}';".format(game[7], game[2])
-            cursor.execute(home_awayxG_query)
-            home_xGa = cursor.fetchall()
-            print("Home team to date xGA: " + str(home_xGa[0][0]))
-
-            away_homexG_query = "SELECT sum(xG) FROM GameEvent e JOIN Game on e.Game = Game.GameId WHERE ConcededTeam = {} AND GameDate < '{}';".format(game[8], game[2])
-            cursor.execute(away_homexG_query)
-            away_xGa = cursor.fetchall()
-            print("Away team to date xGA: " + str(away_xGa[0][0]))
             
 
 
